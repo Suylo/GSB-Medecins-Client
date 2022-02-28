dbd475b84537decad39118965fb27974dd3c9584
Suylo <spoownz@yahoo.com>
2022-02-28 12:35:44 +0100

    Code review

--- src/main/java/fr/suylo/gsbmedecins/controllers/MedecinController.java
+++ src/main/java/fr/suylo/gsbmedecins/controllers/MedecinController.java
@@ -13,16 +13,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
 import lk.vivoxalabs.customstage.CustomStage;
-import lk.vivoxalabs.customstage.CustomStageBuilder;
-import lk.vivoxalabs.customstage.tools.NavigationType;
-import lk.vivoxalabs.customstage.tools.Style;
 
 import java.io.IOException;
 import java.net.URL;

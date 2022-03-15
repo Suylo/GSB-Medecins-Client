e7a3423c476622860b8c41186918b79580ec124d
Suylo <spoownz@yahoo.com>
2022-03-15 13:38:43 +0100

    Added : getAllPaysNom()

--- src/main/java/fr/suylo/gsbmedecins/controllers/department/AddDepartementController.java
+++ src/main/java/fr/suylo/gsbmedecins/controllers/department/AddDepartementController.java
@@ -1,13 +1,6 @@
 package fr.suylo.gsbmedecins.controllers.department;
 
-import com.google.gson.Gson;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import fr.suylo.gsbmedecins.models.Pays;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import fr.suylo.gsbmedecins.models.APIAccess;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -16,8 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AddDepartementController implements Initializable {
@@ -31,26 +22,9 @@ public class AddDepartementController implements Initializable {
     @FXML
     public ComboBox<String> countrySelect;
 
-    public void loadFields() {
-        HttpResponse<JsonNode> apiResponsePays = null;
-        try {
-            apiResponsePays = Unirest.get("http://localhost:8080/api/v1/pays").asJson();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
-        Pays[] lesPays = new Gson().fromJson(String.valueOf(Objects.requireNonNull(apiResponsePays).getBody().toString()), Pays[].class);
-
-        ObservableList<String> data = FXCollections.observableArrayList();
-        for (Pays p : lesPays) {
-            data.add(p.getNom());
-        }
-
-        countrySelect.getItems().addAll(data);
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        loadFields();
+        countrySelect.getItems().addAll(APIAccess.getAllPaysNom());
 
         departmentID.setText("Ajout d'un département");
     }

6174e899e097fb29aa5553186c7ff524d25ce5c1
Suylo <spoownz@yahoo.com>
2022-03-16 16:41:58 +0100

    Removed HTTP Acces in Controller, moved to APIAccess.java

--- src/main/java/fr/suylo/gsbmedecins/controllers/profile/ProfileController.java
+++ src/main/java/fr/suylo/gsbmedecins/controllers/profile/ProfileController.java
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import fr.suylo.gsbmedecins.models.APIAccess;
 import fr.suylo.gsbmedecins.models.Medecin;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -31,25 +32,13 @@ public class ProfileController {
     }
 
     public void loadProfile() {
-        // Récupération d'un seul médecin
-        HttpResponse<JsonNode> apiResponse = null;
-        try {
-            apiResponse = Unirest.get("http://localhost:8080/api/v1/medecins/" + getId().toString()).asJson();
-        } catch (UnirestException e) {
-            e.printStackTrace();
-        }
-        // Récupération d'un médecin au format Json
-        Medecin unMedecin = new Gson().fromJson(String.valueOf(Objects.requireNonNull(apiResponse).getBody().toString()), Medecin.class);
-
-        // Ajout des champs du médecin dans les champs FXML
+        Medecin unMedecin = APIAccess.getMedecinByID(getId());
         doctorID.setText("Profil N°" + unMedecin.getId().toString());
         doctorLastName.setText(unMedecin.getNom());
         doctorName.setText(unMedecin.getPrenom());
         doctorAddress.setText(unMedecin.getAdresse());
         doctorPhone.setText(unMedecin.getTel());
         doctorDepartment.setText(unMedecin.getDepartement().getNom() + " (" + unMedecin.getDepartement().getPays().getNom() + ")");
-
-        // Condition pour vérifier si une spécialité est null
         if (unMedecin.getSpe() == null) {
             doctorSpe.setText("// Spécialité non renseignée.");
         } else {

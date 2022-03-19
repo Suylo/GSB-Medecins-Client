4bb5f52b32e98284316b9d4cb0d20f4e67f10644
Suylo <spoownz@yahoo.com>
2022-03-19 15:36:30 +0100

    Edit doctor is working

--- src/main/java/fr/suylo/gsbmedecins/controllers/profile/EditProfileController.java
+++ src/main/java/fr/suylo/gsbmedecins/controllers/profile/EditProfileController.java
@@ -11,10 +11,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.util.StringConverter;
 import lk.vivoxalabs.customstage.CustomStage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class EditProfileController {
 
@@ -25,11 +25,13 @@ public class EditProfileController {
     @FXML
     public ComboBox<String> doctorSpe;
     @FXML
-    public ComboBox<Integer> doctorDepartment;
+    public ComboBox<Departement> doctorDepartment;
     @FXML
     public Button buttonSave;
 
     private Integer id;
+    private Integer valueDepartment;
+    private String speIsNull = "// Aucune spécialité complémentaire.";
 
     public void loadData(Integer index) {
         this.setId(index);
@@ -50,16 +52,16 @@ public class EditProfileController {
         Medecin unMedecin = APIAccess.getMedecinByID(getId());
 
         ObservableSet<String> uniqueData = FXCollections.observableSet();
-        uniqueData.add("");
+        uniqueData.add(this.speIsNull);
         for (Medecin m : lesMedecins) {
             if (m.getSpe() != null){
                 uniqueData.add(m.getSpe());
             }
         }
 
-        ObservableList<Integer> departements = FXCollections.observableArrayList();
+        ObservableList<Departement> departements = FXCollections.observableArrayList();
         for (Departement de : lesDepartements) {
-            departements.addAll(de.getId());
+            departements.addAll(new Departement(de.getId(), de.getNom()));
         }
 
         doctorID.setText("Modification du profil N°" + unMedecin.getId().toString());
@@ -68,11 +70,31 @@ public class EditProfileController {
         doctorAddress.setText(unMedecin.getAdresse());
         doctorPhone.setText(unMedecin.getTel());
         doctorSpe.getItems().addAll(uniqueData);
-        doctorSpe.setValue(unMedecin.getSpe());
+        doctorDepartment.setValue(unMedecin.getDepartement());
+        if (unMedecin.getSpe() == null){
+            doctorSpe.setValue(this.speIsNull);
+        } else {
+            doctorSpe.setValue(unMedecin.getSpe());
+        }
         doctorDepartment.getItems().addAll(departements);
+        doctorDepartment.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(Departement object) {
+                return object.getNom();
+            }
+
+            @Override
+            public Departement fromString(String string) {
+                return doctorDepartment.getItems().stream().filter(ap -> ap.getNom().equals(string)).findFirst().orElse(null);
+            }
+        });
+        doctorDepartment.valueProperty().addListener((obs, oldval, newval) -> {
+            if (newval != null) {
+                this.valueDepartment = newval.getId();
+            }
+        });
 
         buttonSave.setOnAction(event -> {
-
             Pane doctorEditedNewList = null;
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("doctors.fxml"));
             try {
@@ -84,14 +106,14 @@ public class EditProfileController {
             CustomStage stage = ((CustomStage) buttonSave.getScene().getWindow());
             stage.setTitle("GSB - Liste des médecins");
             stage.changeScene(doctorEditedNewList);
+            if(doctorSpe.getValue().equals(this.speIsNull)){
+                doctorSpe.setValue(null);
+            }
 
-            APIAccess.updateMedecin(
-                    unMedecin.getId(),
-                    doctorLastName,
-                    doctorName,
-                    doctorAddress,
-                    doctorPhone,
-                    doctorSpe);
+            System.out.println("TEST DEBUG VALUE ID :" + doctorDepartment.getValue().getNom());
+            ObservableList<Integer> valueID = APIAccess.getDepartementByNom(doctorDepartment.getValue().getNom());
+            System.out.println("TEST DEBUG UPDATE MEDECIN");
+            APIAccess.updateMedecin(unMedecin.getId(), doctorLastName, doctorName, doctorAddress, doctorPhone, doctorSpe, valueID.get(0));
 
             MedecinController medecinController = loader.getController();
             medecinController.reload();
